73034ea585182d128b19aa984b5602eddf4877dc
jooooscha <57965027+jooooscha@users.noreply.github.com>
2022-11-05 20:31:40 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-hub release create -a ./${APP_FOLDER}/build/outputs/apk/release/*-release-*.apk -m "${GITHUB_REF##*/}" ${GITHUB_REF##*/} 
+hub release create -a ./${APP_FOLDER}/release/*-release-*.apk -m "${GITHUB_REF##*/}" ${GITHUB_REF##*/} 
