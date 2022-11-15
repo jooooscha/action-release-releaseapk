50c9e6b84bc448c519c76ea8815ac0e677167546
jooooscha <57965027+jooooscha@users.noreply.github.com>
2022-11-15 13:46:43 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-hub release create -a ./app/build/outputs/apk/release/app-release-unsigned.apk -m "${GITHUB_REF##*/}" ${GITHUB_REF##*/} 
+# hub release create -a ./app/build/outputs/apk/release/app-release-unsigned.apk -m "${GITHUB_REF##*/}" ${GITHUB_REF##*/} 
+gh release create -d --generate-notes --latest --target main -t test-release-do-not-usel ./app/build/outputs/apk/release/app-release-unsigned.apk
